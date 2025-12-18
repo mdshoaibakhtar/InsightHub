0e7d537958d1859812eaeddd03a91ad2a6d6c4e2
Md Shoaib <shoaib.akhtar@cloudkaptan.com>
2025-12-18 10:26:06 +0530

    fixed bar issue and load page

--- src/shared/charts/BarCharts.tsx
+++ src/shared/charts/BarCharts.tsx
@@ -7,7 +7,7 @@ import {
   Legend,
   LineElement,
   PointElement,
-} from 'chart.js';
+} from 'chart.js/auto';
 import { Chart } from 'react-chartjs-2';
 
 import NoDataFound from '../noDataFound/NoDataFoundPrimary';
