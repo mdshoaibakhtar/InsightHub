dfa8b469483136a6f053780941997007e1f60968
Md Shoaib <mdshoaibakhtar966@gmail.com>
2026-04-25 09:33:26 +0530

    test

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -158,7 +158,7 @@ const data = {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
+    <Sidebar collapsible="offcanvas" {...props}>
       <SidebarHeader className="min-h-16">
         <TeamSwitcher teams={data.teams} />
       </SidebarHeader>
