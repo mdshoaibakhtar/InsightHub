304c26949879d9253cdb1d62c0fc8280a0ff3bd9
Md Shoaib <mdshoaibakhtar966@gmail.com>
2026-04-27 17:42:58 +0530

    Added login page and update app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import {
   SidebarInset,
   SidebarProvider,
@@ -7,26 +6,45 @@ import {
 import { AppSidebar } from "./components/AppSidebar"
 import { SiteHeader } from "./components/SiteHeader"
 import { TooltipProvider } from "./components/ui/tooltip"
-import Home from "./components/screens/Home"
 
-export default function Page() {
+import Home from "./components/screens/Home"
+import LoginScreen from "./components/screens/Login"
+
+import { Routes, Route, Outlet } from "react-router-dom"
+
+function AppLayout() {
   const sidebarStyle = {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 14)",
-        } as React.CSSProperties
+    "--sidebar-width": "calc(var(--spacing) * 72)",
+    "--header-height": "calc(var(--spacing) * 14)",
+  } as React.CSSProperties
 
   return (
     <TooltipProvider>
-    <SidebarProvider style={sidebarStyle}>
-      <AppSidebar variant="inset" />
-      {/* CONTENT AREA */}
-      <SidebarInset>
-        {/* HEADER */}
-        <SiteHeader />
-        {/* DYNAMIC PAGE RENDERING */}
-        <Home/>
-      </SidebarInset>
-    </SidebarProvider>
+      <SidebarProvider style={sidebarStyle}>
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          <Outlet />
+        </SidebarInset>
+      </SidebarProvider>
     </TooltipProvider>
+  )
+}
+
+function AuthLayout() {
+  return <Outlet />
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginScreen />} />
+      </Route>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+
+    </Routes>
   )
 }
